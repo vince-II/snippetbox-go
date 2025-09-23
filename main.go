ec58b852b8cf53eb2ae6c7e617b56e42a0a4513d
vince-II <arnievincent.nery@gmail.com>
2025-09-23 23:04:49 +1000

    added routes

--- main.go
+++ main.go
@@ -7,17 +7,33 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// check request URL if path only "/"
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Createa a new snippet..."))
 }
 
 func main() {
 	fmt.Println("Hello World")
 
+	// routes
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
-
 }
