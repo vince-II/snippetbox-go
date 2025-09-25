5d080355aad54cfc80bbdfdfe2d6c140944c84f5
vince-II <arnievincent.nery@gmail.com>
2025-09-25 16:07:52 +1000

    query string + header metadata

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // https://pkg.go.dev/net/http#pkg-constants
@@ -18,13 +19,43 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+// programing
+// /snippet/view?id=123
 func snippetView(w http.ResponseWriter, r *http.Request) {
+	//extract values id parameter from query string
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Display a specific snippet"))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
+		// Set a new cache-control header. If an existing "Cache-Control" header exists
+		// it will be overwritten.
+		w.Header().Set("Cache-Control", "public, max-age=31536000")
+		// In contrast, the Add() method appends a new "Cache-Control" header and can
+		// be called multiple times.
+		w.Header().Add("Cache-Control", "public")
+		w.Header().Add("Cache-Control", "max-age=31536000")
+
+		// Delete all values for the "Cache-Control" header.
+		w.Header().Del("Cache-Control")
+
+		// Retrieve the first value for the "Cache-Control" header.
+		w.Header().Get("Cache-Control")
+
+		// Retrieve a slice of all values for the "Cache-Control" header.
+		w.Header().Values("Cache-Control")
+
 		w.Header().Set("Allow", http.MethodPost)
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"name": "Alex"}`))
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
