ecc967f0f603db95f63273b236b3b20a58f35d6a
vince-II <arnievincent.nery@gmail.com>
2025-09-26 23:19:08 +1000

    wip os env variable

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// go run ./cmd/web -addr=":9999"
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	addr := os.Getenv("SNIPPETBOX_ADDR")
+	//
+	flag.Parse()
 	mux := http.NewServeMux()
 
 	// file server that serves static
@@ -22,8 +29,8 @@ func main() {
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	mux.HandleFunc("/snippet/download", downloadHandler)
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 
 }
