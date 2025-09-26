b831334e188c6471e99a5a57f940f6940e1652f9
vince-II <arnievincent.nery@gmail.com>
2025-09-26 17:11:31 +1000

    improve rendering of template + add base template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,15 +2,40 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+	"text/template"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	// error handler for endpoint
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
+
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/pages/home.html",
+	}
+	// read the template file and convert to template set
+	ts, err := template.ParseFiles(files...)
+
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	// set to write the template content as the response body
+	// the last parameter, dynamic data
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
