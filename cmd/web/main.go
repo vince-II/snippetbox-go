19f5f7a89456a3422e3d05399205eb2a783c866f
vince-II <arnievincent.nery@gmail.com>
2025-09-28 10:20:26 +1000

    logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,12 +10,13 @@ import (
 func main() {
 	// go run ./cmd/web -addr=":9999"
 	addr := flag.String("addr", ":4000", "HTTP network address")
-	addr := os.Getenv("SNIPPETBOX_ADDR")
-	//
-	flag.Parse()
-	mux := http.NewServeMux()
 
-	// file server that serves static
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	mux := http.NewServeMux()
 	fs := http.FileServer(http.Dir("./ui/static/"))
 
 	// mux.Handle -> register file server as handler for all URL paths with
@@ -29,8 +30,15 @@ func main() {
 	mux.HandleFunc("/snippet/create", snippetCreate)
 	mux.HandleFunc("/snippet/download", downloadHandler)
 
-	log.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+	// log.Printf("Starting server on %s", *addr)
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 
 }
