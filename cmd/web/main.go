bbfab86261e616df26fba446ff02265df0a6ea97
vince-II <arnievincent.nery@gmail.com>
2025-10-03 17:14:08 +1000

    added caching on templates

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"flag"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -13,14 +14,14 @@ import (
 
 // objects
 type application struct {
-	errorLog *log.Logger
-	infoLog  *log.Logger
-	snippets *models.SnippetModel
+	errorLog      *log.Logger
+	infoLog       *log.Logger
+	snippets      *models.SnippetModel
+	templateCache map[string]*template.Template
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
-
 	dsn := flag.String("dsn", "web:pass@tcp(localhost:3306)/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
@@ -33,11 +34,19 @@ func main() {
 	}
 
 	defer db.Close()
+	// Initialize a new template cache...
 
+	templateCache, err := newTemplateCache()
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	// And add it to the application dependencies.
 	app := &application{
-		errorLog: errorLog,
-		infoLog:  infoLog,
-		snippets: &models.SnippetModel{DB: db},
+		errorLog:      errorLog,
+		infoLog:       infoLog,
+		snippets:      &models.SnippetModel{DB: db},
+		templateCache: templateCache,
 	}
 
 	srv := &http.Server{
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,42 @@
 package main
 
-import "github.com/vince-II/go-snippetcode.git/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/vince-II/go-snippetcode.git/internal/models"
+)
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	// Initialize a new map to act as the cache.
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.html")
+	if err != nil {
+		return nil, err
+	}
+	// Loop through the page filepaths one-by-one.
+	for _, page := range pages {
+		name := filepath.Base(page)
+		// Create a slice containing the filepaths for our base template, any
+		// partials and the page.
+		files := []string{
+			"./ui/html/base.html",
+			"./ui/html/partials/nav.html",
+			page,
+		}
+		// Parse the files into a template set.
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+	return cache, nil
 }
