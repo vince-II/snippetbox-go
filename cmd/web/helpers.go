0ecc858b6b1985873be6ac521442f6de81457bf6
vince-II <arnievincent.nery@gmail.com>
2025-10-08 21:06:50 +1100

    improve catching runtine error

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 	"runtime/debug"
@@ -26,17 +27,20 @@ func (app *application) notFound(w http.ResponseWriter) {
 
 func (app *application) render(w http.ResponseWriter, status int, page string, data *templateData) {
 	ts, ok := app.templateCache[page]
-
 	if !ok {
 		err := fmt.Errorf("template %s does not exist", page)
 		app.serverError(w, err)
 		return
 	}
 
-	w.WriteHeader(status)
+	buf := new(bytes.Buffer)
 
-	err := ts.ExecuteTemplate(w, "base", data)
+	err := ts.ExecuteTemplate(buf, "base", data)
 	if err != nil {
 		app.serverError(w, err)
 	}
+
+	w.WriteHeader(status)
+
+	buf.WriteTo(w)
 }
